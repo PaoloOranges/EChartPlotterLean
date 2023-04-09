aaf30a6fb6eb5a7c1c4d591fafbef165ceb66787
Paolo Oranges <paolo.oranges@gmail.com>
2023-04-09 11:22:35 +0200

    remove markers

--- scripts/chart.js
+++ scripts/chart.js
@@ -184,43 +184,6 @@ option = {
         borderColor: upBorderColor,
         borderColor0: downBorderColor
       },
-      markPoint: {
-        label: {
-          formatter: function (param) {
-            return param != null ? Math.round(param.value) + '' : '';
-          }
-        },
-        data: [
-          {
-            name: 'Mark',
-            coord: ['2013/5/31', 2300],
-            value: 2300,
-            itemStyle: {
-              color: 'rgb(41,60,85)'
-            }
-          },
-          {
-            name: 'highest value',
-            type: 'max',
-            valueDim: 'highest'
-          },
-          {
-            name: 'lowest value',
-            type: 'min',
-            valueDim: 'lowest'
-          },
-          {
-            name: 'average value on close',
-            type: 'average',
-            valueDim: 'close'
-          }
-        ],
-        tooltip: {
-          formatter: function (param) {
-            return param.name + '<br>' + (param.data.coord || '');
-          }
-        }
-      },
       markLine: {
         symbol: ['none', 'none'],
         data: [
