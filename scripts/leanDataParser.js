afda929045d9f3a46c85ff7ebe84e43c0f9e171a
Paolo Oranges <paolo.oranges@gmail.com>
2023-04-10 17:03:02 +0200

    creating class parser

--- scripts/leanDataParser.js
+++ scripts/leanDataParser.js
@@ -17,4 +17,71 @@ symbolsKey.forEach(key => {
     priceCharts[symbolKey] = chart;
 });
 
+const compareArrays = (a, b) =>
+  a.length === b.length &&
+  a.every((element, index) => element === b[index]);
+
+let ethEur = priceCharts["ETHEUR"];
+let oTime = ethEur.Series.O.Values.flatMap(v => v.x);
+let hTime = ethEur.Series.H.Values.flatMap(v => v.x);
+let lTime = ethEur.Series.L.Values.flatMap(v => v.x);
+let cTime = ethEur.Series.C.Values.flatMap(v => v.x);
+
+for(var key in ethEur.Series)
+{
+    let timeArray = ethEur.Series[key].Values.flatMap(v => v.x);
+    console.log(timeArray.length);
+    console.log(compareArrays(timeArray, oTime));
+
+}
+
+for(var key in indicators.Series) {
+    let timeArray = indicators.Series[key].Values.flatMap(v => v.x);
+    console.log(timeArray.length);
+    console.log(compareArrays(timeArray, oTime));
+
+  }
+console.log(indicators.Series[0]);
 console.log(indicators);
+
+class LeanDataParser
+{
+    constructor()
+    {
+        this.charts = jsonData.Charts;
+        this.indicators = charts.Indicators;
+
+        var symbolsKey = Object.keys(charts);
+        symbolsKey = symbolsKey.filter(key => key.startsWith(SymbolChartPrefix));
+
+        let priceCharts = {};
+
+        symbolsKey.forEach(key => {
+            symbolKey = key.replace(SymbolChartPrefix, "");
+            let chart = charts[key];
+            chart.Name = symbolKey;
+            priceCharts[symbolKey] = chart;
+        });
+
+        this.priceCharts = priceCharts;
+    }
+
+    getSymbolKeys() 
+    {
+        return Object.keys(this.priceCharts);;
+    }
+
+    getPriceCharts()
+    {
+        return this.priceCharts;
+    }
+
+    getPriceChart(symbol)
+    {
+        return this.priceCharts[symbol];
+    }
+}
+
+let parser = new LeanDataParser();
+let temp1 = parser.getPriceCharts();
+let temp2 = parser.getPriceChart(parser.getSymbolKeys()[0]);
