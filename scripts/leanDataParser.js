f36463b95e0acbbc7427efd7103f2dd1a4179212
Paolo Oranges <paolo.oranges@gmail.com>
2023-04-10 12:13:25 +0200

    extracting prices

--- scripts/leanDataParser.js
+++ scripts/leanDataParser.js
@@ -8,4 +8,13 @@ let indicators = charts.Indicators;
 var symbolsKey = Object.keys(charts);
 symbolsKey = symbolsKey.filter(key => key.startsWith(SymbolChartPrefix));
 
+let priceCharts = {};
+
+symbolsKey.forEach(key => {
+    symbolKey = key.replace(SymbolChartPrefix, "");
+    let chart = charts[key];
+    chart.Name = symbolKey;
+    priceCharts[symbolKey] = chart;
+});
+
 console.log(indicators);
